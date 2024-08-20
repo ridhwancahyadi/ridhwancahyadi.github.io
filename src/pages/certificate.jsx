40e73f1be27618d90a628dbd2753ee40c0087983
Ridhwan Cahyadi <ridhwancahyadi11@gmail.com>
2024-08-20 15:08:36 +0700

    Update certificate

--- src/pages/certificate.jsx
+++ src/pages/certificate.jsx
@@ -19,7 +19,7 @@ const certifications = [
         "Bangkit Academy led by Google, Tokopedia, Gojek, & Traveloka",
       badges: ["TensorFlow", "Mathematics", "Machine Learning", "Deep Learning", "Python", "Data Analytics"],
       imgSrc: "/bangkit.svg",
-      link: "https://github.com/ridhwancahyadi",
+      link: "https://drive.google.com/drive/folders/1x26KWluhVEhGk3JGjkGt1FPoHxWayJFj",
     },
     {
       name: "DeepLearning.AI TensorFlow Developer Specialization",
